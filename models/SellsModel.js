daa8f4a4e75f8d8a8c2bd53130e1f84ff407b65b
Gerardo Delgado <motcrash@gmail.com>
2024-05-19 01:59:44 -0600

    Minimo cambio en el modelo de la tabla

--- models/SellsModel.js
+++ models/SellsModel.js
@@ -4,6 +4,7 @@ import db from '../database/db.js';
 import { DataTypes } from 'sequelize';
 
 const SellsModel = db.define('sells',{
+    sellDate: {type: DataTypes.DATE},
     total: {type: DataTypes.DOUBLE},
     details: {type: DataTypes.JSON},
 });
