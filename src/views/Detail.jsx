5941eac3143fdaa0aac39c4b764d5071c0c69616
Gerardo Delgado <motcrash@gmail.com>
2024-05-22 00:57:57 -0600

    Render bug fixed

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,33 +1,60 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
 import { useLocation } from 'react-router-dom';
-import '../styles/detailStyle.css'
-import HeaderComponent from '../components/HeaderComponent'
-import NavBarComponent from '../components/NavBarComponent'
-
 import axios from 'axios';
+
+import '../styles/detailStyle.css';
+import HeaderComponent from '../components/HeaderComponent';
+import NavBarComponent from '../components/NavBarComponent';
 import LoaderComponent from '../components/LoaderComponent';
-const URISales = 'http://localhost:8000/sells/';
-const URIProducts = 'http://localhost:8000/products/';
+
 
 function Detail() {
   
   let { state } = useLocation();
   const {id} = state;
-  const [loading, setLoading] = useState(true);
+  
+  const URISales = 'http://localhost:8000/sells/';
+  const URIProducts = 'http://localhost:8000/products/';
+  
+  const [isLoading, setIsLoading] = useState(true);
+  
   const [sale, setSale] = useState([]);
-  const [products, setProduct] = useState([]);
   const {details} = sale;
   
-  useEffect(() => {
-    setTimeout(() => {
-      getProduct();
-      getSale();
-      setLoading(false)
-    }, 3000);
-  }, []);
+  const [products, setProduct] = useState([]);
 
+
+  const getSale = async () => {
+    const res = await axios.get(`${URISales}${id}`);
+    setSale(res.data);
+  }
+
+  const getProduct = async () => {
+    const res = await axios.get(`${URIProducts}`);
+    setProduct(res.data);
+  }
+  
+  useEffect(() => {
+    getProduct();
+    getSale();
+    const loadingTime = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(loadingTime);
+  }, []);
+  
+  const getProductName =  (id) => {
+      return products.find(product => product.id === id).title;
+  }
+  const getPrice = (id) => {
+    return products.find(product => product.id === id).price;
+  }
+  const getSubtotal = (id, quantity) => {
+    return (products.find(product => product.id === id).price) * quantity;
+  }
+  
   const renderDetails = (details) => {
-    if (details) {
       const filteredDetails = details.filter(detail => detail.quantity > 0)
       return (filteredDetails.map(detail => (
         <tr key={detail.id}> 
@@ -37,61 +64,41 @@ function Detail() {
          <td>{getSubtotal(detail.id, detail.quantity)}</td>
         </tr>
       )))
-    } 
-  }
-  
-  const getProductName =  (id) => {
-      return products.find(product => product.id === id).title;
-  }
-  const getPrice = (id) => {
-    return products.find(product => product.id === id).price;
-  }
-  const getSubtotal = (id, quantity) => {
-    return (products.find(product => product.id === id).price)* quantity;
-  }
-  
-  const getSale = async () => {
-    const res = await axios.get(`${URISales}${id}`);
-    setSale(res.data);
-  }
-  const getProduct = async () => {
-    const res = await axios.get(`${URIProducts}`);
-    setProduct(res.data);
   }
 
   return (
     <div>
-      {loading ? (<LoaderComponent/>)
-      : (
-        <div>
-        <HeaderComponent />
-        <NavBarComponent />
+    {isLoading ? (<LoaderComponent/>)
+    : (
+      <div>
+      <HeaderComponent />
+      <NavBarComponent />
 
-        {/*Tabla de Detalle*/}
-        <div className='table-detail-container'>
-          <h1>Venta #{id}</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>Nombre</th>
-                <th>Cantidad</th>
-                <th>precio</th>
-                <th>Subtotal</th>
-              </tr>
-            </thead>
-            <tbody>
-            {renderDetails(details)}
+      {/*Tabla de Detalle*/}
+      <div className='table-detail-container'>
+        <h1>Venta #{id}</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Cantidad</th>
+              <th>precio</th>
+              <th>Subtotal</th>
+            </tr>
+          </thead>
+          <tbody>
+          {renderDetails(details)}
 
-            </tbody>
-          </table>
-        </div>
+          </tbody>
+        </table>
+      </div>
 
-        <div className="total-detail">Total: ${sale.total}</div>
-        </div>  
-      )  
-    
-    }
-    </div>
+      <div className="total-detail">Total: ${sale.total}</div>
+      </div>  
+    )  
+  
+  }
+  </div>
   )
 }
 
