e7b754248fcb181eeb4afb3b7a9d6599dc4581d1
Gerardo Delgado <motcrash@gmail.com>
2024-05-22 00:57:46 -0600

    Loader minimal changes

--- src/components/LoaderComponent.jsx
+++ src/components/LoaderComponent.jsx
@@ -1,17 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ClipLoader from "react-spinners/ClipLoader";
 import '../styles/loaderStyle.css'
 
 
 function LoaderComponent() {
 
-    let [loading, setLoading] = useState(true);
-
   return (
     <div className="loaderContainer">
       <ClipLoader
         color={"#0056b3"}
-        loading={loading}
         size={150}
         aria-label="Loading Spinner"
         data-testid="loader"
