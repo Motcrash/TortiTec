2118068dafe591760925bc972255ffbccdd60588
Gerardo Delgado <motcrash@gmail.com>
2024-05-22 00:59:47 -0600

    Changes on db structure

--- database/tortitec_db.sql
+++ database/tortitec_db.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 20, 2024 at 06:10 PM
+-- Host: localhost
+-- Generation Time: May 22, 2024 at 08:58 AM
 -- Server version: 8.0.36
 -- PHP Version: 8.2.12
 
@@ -47,11 +47,11 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `title`, `description`, `price`, `img_source`, `quantity`, `createdAt`, `updatedAt`) VALUES
 (1, '1Kg de tortillas', 'Un kilogramo de tortillas de maíz', 25, '/src/assets/img/tortillas.jpg', 0, '2024-05-15 16:31:05', '2024-05-15 16:58:45'),
-(2, '0.5Kg de tortillas', 'Medio kilogramo de tortillas de maíz', 15, '/src/assets/img/tortillas-2.jpg', 0, '2024-05-15 16:31:05', '2024-05-15 16:58:51'),
-(3, 'Paquete de frijoles', 'Un paquete de frijoles cocidos', 25, '/src/assets/img/frijoles.jpg', 0, '2024-05-15 16:31:05', '2024-05-15 16:59:14'),
+(2, '0.5Kg de tortillas', 'Medio kilogramo de tortillas de maíz', 15, '/src/assets/img/tortillas-2.jpg', 0, '2024-05-15 16:31:05', '2024-05-22 02:17:30'),
+(3, 'Paquete de frijoles', 'Un paquete de frijoles cocidos', 25, '/src/assets/img/frijoles.jpg', 0, '2024-05-15 16:31:05', '2024-05-22 02:17:30'),
 (4, '0.5Kg de Chicharrón', '500gr de chicharrón a granél', 130, '/src/assets/img/chicharron.jpg', 0, '2024-05-15 16:31:05', '2024-05-16 15:30:37'),
-(5, '250gr de Chicharrón', '250gr de chicharrón a granél', 80, '/src/assets/img/chicharron-2.jpg', 0, '2024-05-15 16:31:05', '2024-05-16 15:30:27'),
-(6, 'Conito de cajeta', 'Cono de galleta relleno de cajeta', 20, '/src/assets/img/cajeta.jpg', 0, '2024-05-15 16:52:38', '2024-05-15 16:59:57'),
+(5, '250gr de Chicharrón', '250gr de chicharrón a granél', 80, '/src/assets/img/chicharron-2.jpg', 0, '2024-05-15 16:31:05', '2024-05-22 02:17:30'),
+(6, 'Conito de cajeta', 'Cono de galleta relleno de cajeta', 20, '/src/assets/img/cajeta.jpg', 0, '2024-05-15 16:52:38', '2024-05-22 02:17:30'),
 (8, 'Chiles rellenos', 'Paquete de dos chiles rellenos', 30, '/src/assets/img/chiles.jpg', 0, '2024-05-16 14:57:07', '2024-05-16 14:57:07'),
 (9, 'Requesón', 'Queso tipo requesón de 200gr', 25, '/src/assets/img/requeson.jpg', 0, '2024-05-16 14:57:46', '2024-05-16 14:57:46'),
 (10, 'Chile colorado', 'Paquete de 400ml de chile colorado', 28, '/src/assets/img/chileColorado.jpg', 0, '2024-05-16 14:59:01', '2024-05-16 14:59:01');
@@ -80,14 +80,10 @@ INSERT INTO `sells` (`id`, `sellDate`, `total`, `details`, `createdAt`, `updated
 (34, '2024-05-19 21:22:38', 340, '[{\"id\": 1, \"quantity\": 3}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 2}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 1}, {\"id\": 6, \"quantity\": 1}, {\"id\": 8, \"quantity\": 3}, {\"id\": 9, \"quantity\": 1}, {\"id\": 10, \"quantity\": 0}]', '2024-05-19 21:22:38', '2024-05-19 21:22:38'),
 (35, '2024-05-19 21:23:00', 170, '[{\"id\": 1, \"quantity\": 1}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 1}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 1}, {\"id\": 6, \"quantity\": 2}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 0}]', '2024-05-19 21:23:00', '2024-05-19 21:23:00'),
 (36, '2024-05-19 21:24:36', 196, '[{\"id\": 1, \"quantity\": 2}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 2}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 2}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 2}]', '2024-05-19 21:24:36', '2024-05-19 21:24:36'),
+(37, '2024-05-19 23:21:23', 160, '[{\"id\": 1, \"quantity\": 1}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 0}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 1}, {\"id\": 6, \"quantity\": 0}, {\"id\": 8, \"quantity\": 1}, {\"id\": 9, \"quantity\": 1}, {\"id\": 10, \"quantity\": 0}]', '2024-05-19 23:21:23', '2024-05-19 23:21:23'),
 (38, '2024-05-19 23:21:31', 363, '[{\"id\": 1, \"quantity\": 0}, {\"id\": 2, \"quantity\": 4}, {\"id\": 3, \"quantity\": 1}, {\"id\": 4, \"quantity\": 1}, {\"id\": 5, \"quantity\": 1}, {\"id\": 6, \"quantity\": 2}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 1}]', '2024-05-19 23:21:31', '2024-05-19 23:21:31'),
-(39, '2024-05-20 04:53:01', 216, '[{\"id\": 1, \"quantity\": 1}, {\"id\": 2, \"quantity\": 3}, {\"id\": 3, \"quantity\": 2}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 2}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 2}]', '2024-05-20 04:53:01', '2024-05-20 04:53:01'),
-(40, '2024-05-20 14:34:02', 570, '[{\"id\": 1, \"quantity\": 5}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 2}, {\"id\": 4, \"quantity\": 2}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 0}, {\"id\": 8, \"quantity\": 2}, {\"id\": 9, \"quantity\": 3}, {\"id\": 10, \"quantity\": 0}]', '2024-05-20 14:34:02', '2024-05-20 14:34:02'),
-(41, '2024-05-20 14:34:24', 25, '[{\"id\": 1, \"quantity\": 1}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 0}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 0}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 0}]', '2024-05-20 14:34:24', '2024-05-20 14:34:24'),
-(42, '2024-05-20 14:53:49', 731, '[{\"id\": 1, \"quantity\": 4}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 0}, {\"id\": 4, \"quantity\": 2}, {\"id\": 5, \"quantity\": 3}, {\"id\": 6, \"quantity\": 0}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 3}, {\"id\": 10, \"quantity\": 2}]', '2024-05-20 14:53:49', '2024-05-20 14:53:49'),
-(43, '2024-05-20 14:54:08', 25, '[{\"id\": 1, \"quantity\": 1}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 0}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 0}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 0}]', '2024-05-20 14:54:08', '2024-05-20 14:54:08'),
-(44, '2024-05-20 14:56:57', 75, '[{\"id\": 1, \"quantity\": 2}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 1}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 0}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 0}]', '2024-05-20 14:56:57', '2024-05-20 14:56:57'),
-(45, '2024-05-20 15:01:23', 180, '[{\"id\": 1, \"quantity\": 3}, {\"id\": 2, \"quantity\": 2}, {\"id\": 3, \"quantity\": 3}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 0}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 0}]', '2024-05-20 15:01:23', '2024-05-20 15:01:23');
+(39, '2024-05-22 01:07:19', 270, '[{\"id\": 1, \"quantity\": 0}, {\"id\": 2, \"quantity\": 5}, {\"id\": 3, \"quantity\": 0}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 3}, {\"id\": 8, \"quantity\": 2}, {\"id\": 9, \"quantity\": 3}, {\"id\": 10, \"quantity\": 0}]', '2024-05-22 01:07:19', '2024-05-22 01:07:19'),
+(72, '2024-05-22 03:15:22', 75, '[{\"id\": 1, \"quantity\": 3}, {\"id\": 2, \"quantity\": 0}, {\"id\": 3, \"quantity\": 0}, {\"id\": 4, \"quantity\": 0}, {\"id\": 5, \"quantity\": 0}, {\"id\": 6, \"quantity\": 0}, {\"id\": 8, \"quantity\": 0}, {\"id\": 9, \"quantity\": 0}, {\"id\": 10, \"quantity\": 0}]', '2024-05-22 03:15:22', '2024-05-22 03:15:22');
 
 -- --------------------------------------------------------
 
@@ -108,15 +104,15 @@ CREATE TABLE `stocks` (
 --
 
 INSERT INTO `stocks` (`id`, `quantity`, `createdAt`, `updatedAt`) VALUES
-(1, 6, '2024-05-18 21:41:44', '2024-05-20 15:01:33'),
-(2, 0, '2024-05-18 21:41:44', '2024-05-20 15:01:23'),
-(3, 3, '2024-05-18 21:41:44', '2024-05-20 14:56:57'),
-(4, 7, '2024-05-18 21:41:44', '2024-05-20 14:53:49'),
-(5, 6, '2024-05-18 21:41:44', '2024-05-20 14:53:49'),
-(6, 14, '2024-05-18 21:41:44', '2024-05-20 04:53:01'),
-(8, 13, '2024-05-18 21:41:44', '2024-05-20 14:34:02'),
-(9, 6, '2024-05-18 21:41:44', '2024-05-20 14:53:49'),
-(10, 7, '2024-05-18 21:41:44', '2024-05-20 14:53:49');
+(1, 11, '2024-05-18 21:41:44', '2024-05-22 03:15:22'),
+(2, 17, '2024-05-18 21:41:44', '2024-05-22 03:15:02'),
+(3, 17, '2024-05-18 21:41:44', '2024-05-22 03:15:03'),
+(4, 18, '2024-05-18 21:41:44', '2024-05-22 03:15:04'),
+(5, 20, '2024-05-18 21:41:44', '2024-05-22 03:15:05'),
+(6, 21, '2024-05-18 21:41:44', '2024-05-22 03:15:06'),
+(8, 17, '2024-05-18 21:41:44', '2024-05-22 01:07:19'),
+(9, 13, '2024-05-18 21:41:44', '2024-05-22 03:15:07'),
+(10, 20, '2024-05-18 21:41:44', '2024-05-22 03:15:08');
 
 -- --------------------------------------------------------
 
@@ -139,6 +135,58 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `user`, `password`, `createdAt`, `updatedAt`) VALUES
 (1, 'test', 'test', '2024-05-16 14:25:18', '2024-05-16 14:25:18');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `vw_details`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `vw_details`;
+CREATE TABLE `vw_details` (
+`details` json
+,`id` int
+,`price` double
+,`title` varchar(50)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `vw_main`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `vw_main`;
+CREATE TABLE `vw_main` (
+`createdAt` timestamp
+,`id` int
+,`img_source` varchar(100)
+,`price` double
+,`quantity` int
+,`stock` int
+,`title` varchar(50)
+,`updatedAt` timestamp
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `vw_details`
+--
+DROP TABLE IF EXISTS `vw_details`;
+
+DROP VIEW IF EXISTS `vw_details`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_details`  AS SELECT `products`.`id` AS `id`, `products`.`title` AS `title`, `products`.`price` AS `price`, `sells`.`details` AS `details` FROM (`products` join `sells`) WHERE (json_extract(`sells`.`details`,'$.id') = `products`.`id`) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `vw_main`
+--
+DROP TABLE IF EXISTS `vw_main`;
+
+DROP VIEW IF EXISTS `vw_main`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_main`  AS SELECT `products`.`id` AS `id`, `products`.`title` AS `title`, `products`.`price` AS `price`, `products`.`quantity` AS `quantity`, `products`.`img_source` AS `img_source`, `stocks`.`quantity` AS `stock`, `products`.`createdAt` AS `createdAt`, `products`.`updatedAt` AS `updatedAt` FROM (`products` join `stocks`) WHERE (`stocks`.`id` = `products`.`id`) ;
 
 --
 -- Indexes for dumped tables
@@ -170,13 +218,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `sells`
 --
 ALTER TABLE `sells`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `users`
