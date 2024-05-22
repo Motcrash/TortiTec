c4eff9d31c603b210a032fca26fcf2da45091064
Gerardo Delgado <motcrash@gmail.com>
2024-05-21 19:25:32 -0600

    db changes ignored

--- database/db.js
+++ database/db.js
@@ -1,8 +1,7 @@
 import {Sequelize} from 'sequelize';
 
-const db = new Sequelize('tortitec_db', 'root', '', {
+const db = new Sequelize('tortitec_db', 'root', 'root', {
     host: 'localhost',
-    port: 3308,
     dialect: 'mysql'
 });
 
