b34be1deb21bc58836d4153336b7f26f5b90b017
Gerardo Delgado <motcrash@gmail.com>
2024-05-23 11:21:34 -0600

    activate/deactivate products implemented

--- src/views/Stock.jsx
+++ src/views/Stock.jsx
@@ -3,6 +3,7 @@ import '../styles/stockStyle.css'
 import HeaderComponent from '../components/HeaderComponent'
 import NavBarComponent from '../components/NavBarComponent'
 import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 const URIStock = 'http://localhost:8000/stocks/';
 const URIProducts = 'http://localhost:8000/products/';
@@ -16,46 +17,76 @@ function Stock() {
       getStock();
       getProduct();
     }, []);
+
+    const noifyDeactivate = () => toast.success('Producto desactivado con exito');
+    const notifyActivate = () => toast.success('Producto activado con exito');
     
     const getStock = async () => {
         const res = await axios.get(URIStock);
         setStock(res.data);
-    }
-
-    const getProduct = async () => {
+      }
+      
+      const getProduct = async () => {
         const res = await axios.get(URIProducts);
         setProduct(res.data);
     }
 
-    const renderProduct = (products) => {
-      if(products[0] != undefined){
-        return (stock.map(item => (
-          <tr key={item.id}>
-            <td>{products.find(product => product.id === item.id).title}</td>
-            <td>{item.quantity}</td>
-            <td>
-            <div className='buttons-stock'>
-            <button className='detailButton' onClick={() => handleIncrease(item)}>
-              <img src='/src/assets/img/mas.png' alt="moreButton"/>
-            </button>
-            <button onClick={() => handleDecrease(item)}>
-              <img src='/src/assets/img/menos.png' alt="moreButton"/>
-            </button>
-            </div>
-            </td>
-          </tr>
-        )))
-      }
+    const renderActiveProducts = (products) => {
+      return (stock.map(item => {
+        if (item.isActive == true) { return(
+            <tr key={item.id}>
+              <td>{products.find(product => product.id === item.id).title}</td>
+              <td>{item.quantity}</td>
+              <td>
+              <div className='buttons-stock'>
+              <button className='detailButton' onClick={() => handleIncrease(item)}>
+                <img src='/src/assets/img/mas.png' alt="moreButton"/>
+              </button>
+              <button onClick={() => handleDecrease(item)}>
+                <img src='/src/assets/img/menos.png' alt="moreButton"/>
+              </button>
+              </div>
+              </td>
+              <td>
+              <div className='buttons-stock'>
+              <button className='detailButton' onClick={() => deactivateProduct(item)}>
+                <img src='/src/assets/img/activar.png' alt="moreButton"/>
+              </button>
+              </div>
+              </td>
+            </tr>
+          )
+        }
+      }))
     }
 
-    const handleIncrease = (item) => {
+    const renderInactiveProducts = (products) => {
+      return (stock.map(item => {
+        if (item.isActive == false) { return(
+            <tr key={item.id}>
+              <td>{products.find(product => product.id === item.id).title}</td>
+              <td>{item.quantity}</td>
+              <td>
+              <div className='buttons-stock'>
+              <button className='detailButton' onClick={() => activateProduct(item)}>
+                <img src='/src/assets/img/desactivar.png' alt="moreButton"/>
+              </button>
+              </div>
+              </td>
+            </tr>
+          )
+        }
+      }))
+    }
+
+    const handleIncrease = async (item) => {
         const updatedItems = stock.map((i) =>
           i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
         );
         setStock(updatedItems);
 
         if(item.quantity < 200){
-          axios.put(`${URIStock}${item.id}`, {
+          await axios.put(`${URIStock}${item.id}`, {
             quantity: item.quantity + 1
           })
         }
@@ -69,10 +100,34 @@ function Stock() {
         setStock(updatedItems);
 
         if(item.quantity > 0){
-          axios.put(`${URIStock}${item.id}`, {
+          await axios.put(`${URIStock}${item.id}`, {
             quantity: item.quantity - 1
           })
         }
+      };
+
+      const deactivateProduct = async (item) => {
+        const updatedItems = stock.map((i) =>
+          i.id === item.id
+          ? { ...i, isActive: false } : i
+        );
+        setStock(updatedItems);
+        await axios.put(`${URIStock}${item.id}`, {
+          isActive: false
+        })
+        noifyDeactivate();
+      };
+
+      const activateProduct = async (item) => {
+        const updatedItems = stock.map((i) =>
+          i.id === item.id
+          ? { ...i, isActive: true } : i
+        );
+        setStock(updatedItems);
+        await axios.put(`${URIStock}${item.id}`, {
+          isActive: true
+        })
+        notifyActivate();
       };
       
       
@@ -81,22 +136,38 @@ function Stock() {
     <HeaderComponent />
     <NavBarComponent /> 
 
-        {/*Tabla*/}
-        <div className='table-stock-container'>
-          <h1>Inventario</h1>
+      {/*Tabla*/}
+      <div className='table-stock-container'>
+      <h1>Inventario</h1>
       <table>
         <thead>
           <tr>
             <th>Producto</th>
             <th>Cantidad</th>
             <th>Control de Inventario</th>
+            <th>Descontinuar</th>
           </tr>
         </thead>
         <tbody>
-          {renderProduct(products)}
+          {renderActiveProducts(products)}
+        </tbody>
+      </table>
+
+      <h2>Productos descontinuados</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Producto</th>
+            <th>Cantidad</th>
+            <th>activar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderInactiveProducts(products)}
         </tbody>
       </table>
     </div>
+    <Toaster />
     </div>
        
   )
