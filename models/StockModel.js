8f20882b85aa504191dc0870520c2cc2a0aa9b9f
Gerardo Delgado <motcrash@gmail.com>
2024-05-23 11:20:24 -0600

    StockModel Changed

--- models/StockModel.js
+++ models/StockModel.js
@@ -3,7 +3,8 @@ import db from '../database/db.js';
 import { DataTypes } from 'sequelize';
 
 const StockModel = db.define('stocks', {
-    quantity: {type: DataTypes.INTEGER}
+    quantity: {type: DataTypes.INTEGER},
+    isActive: {type: DataTypes.BOOLEAN},
 });
 
 export default StockModel;
