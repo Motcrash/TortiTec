c6c472e424a58d28be309bc5f12ca945444dcd12
Darth_Yisus2099 <jesusalejandrot958@gmail.com>
2024-05-28 18:50:55 -0600

    Update Login

--- src/views/LoginError.jsx
+++ src/views/LoginError.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import HeaderComponent from '../components/HeaderComponent';
-
+import '../styles/loginError.css'
 function LoginError() {
   return (
     <div>
         <HeaderComponent />
-        <h1>ERROR PAPU</h1>
+        <h1 className='header1-error'>Inicia sesión para acceder a la aplicación</h1>
     </div>
   )
 }
