5fa38ab788b46d4e40cafd3bef1cece3e7e28679
Darth_Yisus2099 <jesusalejandrot958@gmail.com>
2024-05-28 18:28:17 -0600

    Se crea vista de LoginError

---
+++ src/views/LoginError.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderComponent from '../components/HeaderComponent';
+
+function LoginError() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default LoginError
