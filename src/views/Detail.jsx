6fb66dc797eeab5cc8c3a1ce0a6b292eab93d69c
Gerardo Delgado <motcrash@gmail.com>
2024-06-29 21:26:50 -0600

    Detail logic changes

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,105 +1,83 @@
-import React, {useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import axios from 'axios';
 
+import { SellsContext } from '../context/SellsContext';
 import '../styles/detailStyle.css';
 import HeaderComponent from '../components/HeaderComponent';
 import NavBarComponent from '../components/NavBarComponent';
 import LoaderComponent from '../components/LoaderComponent';
 
+const notifyError = () => toast.error("Ocurrió un error al cargar los datos", { id: "dataError", duration: 1000 });
 
 function Detail() {
-  
+  const { sells, isLoading } = useContext(SellsContext);
+  const [sell, setSell] = useState({ details: [] });
+
   let { state } = useLocation();
-  const {id} = state;
-  
-  const URISales = 'http://localhost:8000/sells/';
-  const URIProducts = 'http://localhost:8000/products/';
-  
-  const [isLoading, setIsLoading] = useState(true);
-  
-  const [sale, setSale] = useState([]);
-  const {details} = sale;
-  
-  const [products, setProduct] = useState([]);
+  const { id } = state;
 
-
-  const getSale = async () => {
-    const res = await axios.get(`${URISales}${id}`);
-    setSale(res.data);
+  const getSell = () => {
+    try {
+      const uniqueSell = sells.find(sell => sell.id === id);
+      if (uniqueSell) {
+        setSell(uniqueSell);
+      } else {
+        notifyError();
+      }
+    } catch (error) {
+      notifyError();
+    }
   }
 
-  const getProduct = async () => {
-    const res = await axios.get(`${URIProducts}`);
-    setProduct(res.data);
-  }
-  
   useEffect(() => {
-    getProduct();
-    getSale();
-    const loadingTime = setTimeout(() => {
-      setIsLoading(false);
-    }, 200);
+    if (!isLoading) {
+      getSell();
+    }
+  }, [isLoading, sells]);
 
-    return () => clearTimeout(loadingTime);
-  }, []);
-  
-  const getProductName =  (id) => {
-      return products.find(product => product.id === id).title;
-  }
-  const getPrice = (id) => {
-    return products.find(product => product.id === id).price;
-  }
-  const getSubtotal = (id, quantity) => {
-    return (products.find(product => product.id === id).price) * quantity;
-  }
-  
-  const renderDetails = (details) => {
-      return (details.map(detail => (
-        <tr key={detail.id}> 
-          <td>{getProductName(detail.id)}</td>
-          <td>{detail.quantity}</td>
-          <td>{getPrice(detail.id)}</td>
-         <td>{getSubtotal(detail.id, detail.quantity)}</td>
-        </tr>
-      )))
+  const renderDetails = () => {
+    return sell.details.map(detail => (
+      <tr key={detail.title}>
+        <td>{detail.title}</td>
+        <td>{detail.quantity}</td>
+        <td>{detail.price}</td>
+        <td>{detail.price * detail.quantity}</td>
+      </tr>
+    ));
   }
 
   return (
-    <div>
-    {isLoading ? (<LoaderComponent/>)
-    : (
-      <div>
-      <HeaderComponent />
-      <NavBarComponent />
+    <>
+      {isLoading ? (
+        <LoaderComponent />
+      ) : (
+        <div>
+          <HeaderComponent />
+          <NavBarComponent />
 
-      {/*Tabla de Detalle*/}
-      <div className='table-detail-container'>
-        <h1>Venta #{id}</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Cantidad</th>
-              <th>Precio</th>
-              <th>Subtotal</th>
-            </tr>
-          </thead>
-          <tbody>
-          {renderDetails(details)}
+          {/* Tabla de Detalle */}
+          <div className='table-detail-container'>
+            <h1>Venta #{id}</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Cantidad</th>
+                  <th>Precio</th>
+                  <th>Subtotal</th>
+                </tr>
+              </thead>
+              <tbody>
+                {sell.details.length > 0 ? renderDetails() : <tr><td colSpan="4">No hay detalles disponibles</td></tr>}
+              </tbody>
+            </table>
+          </div>
 
-          </tbody>
-        </table>
-      </div>
-
-      <div className="total-detail">Total: ${sale.total}</div>
-      </div>  
-    )  
-  
-  }
-  </div>
-  )
+          <div className="total-detail">Total: ${sell.total || 0}</div>
+        </div>
+      )}
+    </>
+  );
 }
 
-
-export default Detail
+export default Detail;
