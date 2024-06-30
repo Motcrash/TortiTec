fc8304f417c1682545e270fe1598ea3a034c7284
Gerardo Delgado <motcrash@gmail.com>
2024-06-29 21:25:04 -0600

    Minimal QoL change

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -8,6 +8,7 @@ const Product = ({ product, onIncrease, onDecrease }) => {
       <h3>{product.name}</h3>
       <p>{product.price}</p>
       <p>Cantidad: {product.quantity}</p>
+      <p>Stock: {product.stock}</p>
       <button onClick={() => onIncrease(product)}>Aumentar</button>
       <button onClick={() => onDecrease(product)}>Quitar</button>
     </div>
