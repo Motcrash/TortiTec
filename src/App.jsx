64c111fa82d12614ef2a2a220707ad9277ff4720
Gerardo Delgado <motcrash@gmail.com>
2024-06-29 21:22:42 -0600

    Modified paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Login from './views/Login';
 import Main from "./views/Main";
 import Stock from "./views/Stock";
-import Sales from "./views/Sales";
+import Sells from "./views/Sells.jsx";
 import Detail from "./views/Detail";
 import LoginError from "./views/LoginError";
-// import Edit from "./views/Edit";
-import ProtectedRoute from './components/ProtectedRouteComponent';
+import AddProduct from "./views/AddProduct";
+import EditProduct from "./views/EditProduct";
+
+import { ProductsProvider } from './context/ProductsContext.jsx'
+import { SellsProvider } from "./context/SellsContext.jsx";
 
 function App() {
   const [isLogIn, setLogIn] = useState(false);
@@ -21,17 +24,22 @@ function App() {
   }
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login login={login} logout={logout}/>} />
-        <Route path="/loginError" element={<LoginError />}/>
-        <Route path="/main" element={<ProtectedRoute element={<Main />} isAuthenticated={isLogIn} /> } />
-        <Route path="/stock" element={<ProtectedRoute element={<Stock />} isAuthenticated={isLogIn} />} />
-        <Route path="/sales" element={<ProtectedRoute element={<Sales />} isAuthenticated={isLogIn} />} />
-        <Route path="/detail/:id" element={<ProtectedRoute element={<Detail />} isAuthenticated={isLogIn} />} />
-        {/* <Route path="/edit_product/:id" element={<Edit />} /> */}
-      </Routes>
-    </Router>
+      <SellsProvider>
+        <ProductsProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Login login={login} logout={logout}/>} />
+              <Route path="/loginError" element={<LoginError />}/>
+              <Route path="/main" element={<Main />}  />
+              <Route path="/stock" element={<Stock />} />
+              <Route path="/sells" element={<Sells />} />
+              <Route path="/detail/:id" element={<Detail />} />
+              <Route path="/add_product/" element={<AddProduct />} />
+              <Route path="/edit_product/:id" element={<EditProduct />} />
+            </Routes>
+          </Router>
+        </ProductsProvider>
+      </SellsProvider>
   );
 }
 
